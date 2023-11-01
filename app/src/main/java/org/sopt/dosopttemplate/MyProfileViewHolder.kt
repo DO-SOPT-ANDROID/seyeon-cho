6664c582ac0ccae1d38ff3fadd5deef395412fd4
crownjoe <crownjoe@naver.com>
2023-11-01 16:40:10 +0900

    ViewHolder에 Glide 사용2

--- app/src/main/java/org/sopt/dosopttemplate/MyProfileViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/MyProfileViewHolder.kt
@@ -1,6 +1,7 @@
 package org.sopt.dosopttemplate
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import org.sopt.dosopttemplate.databinding.ItemFriendBinding
 import org.sopt.dosopttemplate.databinding.ItemMyprofileBinding
 
@@ -8,9 +9,12 @@ class MyProfileViewHolder(private val binding: ItemMyprofileBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(userUserProfileData: UserProfile.My) {
-        binding.ivProfile.setImageResource(userUserProfileData.profileImage)
-        binding.tvName.text= userUserProfileData.name
-        binding.tvSelfMessage.text= userUserProfileData.message
+        with(binding) {
+            Glide.with(ivProfile)
+                .load(userUserProfileData.profileImage)
+                .into(ivProfile)
+            tvName.text = userUserProfileData.name
+            tvSelfMessage.text = userUserProfileData.message
+        }
     }
-
 }
