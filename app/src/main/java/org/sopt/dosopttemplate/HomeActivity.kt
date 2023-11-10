f23443e79a6d4229bf8b4a446a36c0afa1b52923
crownjoe <crownjoe@naver.com>
2023-11-10 16:02:40 +0900

    [ADD] screencapture

--- app/src/main/java/org/sopt/dosopttemplate/HomeActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/HomeActivity.kt
@@ -1,29 +1,44 @@
 package org.sopt.dosopttemplate
 
 import android.animation.ObjectAnimator
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import org.sopt.dosopttemplate.databinding.ActivityHomeBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import android.Manifest
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private var openFAB = false
+    private val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_home)
-        if (currentFragment == null){
+        if (currentFragment == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.fcv_home, HomeFragment())
                 .commit()
         }
         clickBottomNavigation()
         setCLICKFAB()
+        checkManageExternalStoragePermission()
     }
 
     companion object {
@@ -36,15 +51,15 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-    private fun clickBottomNavigation(){
+    private fun clickBottomNavigation() {
         binding.bnvHome.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.menu_home-> {
+                R.id.menu_home -> {
                     replaceFragment(HomeFragment())
                     true
                 }
 
-                R.id.menu_do_android-> {
+                R.id.menu_do_android -> {
                     replaceFragment(DoAndroidFragment())
                     true
                 }
@@ -73,12 +88,12 @@ class HomeActivity : AppCompatActivity() {
 
     }
 
-    private fun setCLICKFAB(){
+    private fun setCLICKFAB() {
         binding.fabMain.setOnClickListener {
             eventFAB()
         }
         binding.fabCapture.setOnClickListener {
-            Toast.makeText(this, "캡쳐하는 중", Toast.LENGTH_SHORT).show()
+            takeScreenshot()
         }
 
         binding.fabShare.setOnClickListener {
@@ -86,8 +101,8 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-    private fun eventFAB(){
-        if (openFAB){
+    private fun eventFAB() {
+        if (openFAB) {
             ObjectAnimator.ofFloat(binding.fabShare, "translationY", 0f).apply { start() }
             ObjectAnimator.ofFloat(binding.fabCapture, "translationY", 0f).apply { start() }
         } else {
@@ -96,6 +111,64 @@ class HomeActivity : AppCompatActivity() {
         }
         openFAB = !openFAB
     }
-}
 
+    private fun checkManageExternalStoragePermission() {
+        val managePermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        if (managePermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                WRITE_EXTERNAL_STORAGE_REQUEST_CODE
+            )
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "권한이 허용되었습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun takeScreenshot() {
+        val rootView = window.decorView.rootView
+        val screenShot: File? = captureScreen(rootView)
+    }
+    private fun captureScreen(view: View): File? {
+        val screenBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(screenBitmap)
+        view.draw(canvas)
+
+        if (screenBitmap.byteCount == 0) {
+            return null
+        }
+
+        val filename = "Profilescreenshot.png"
+        val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), filename)
+
+        try {
+            val output = FileOutputStream(file)
+            screenBitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
+            output.close()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+
+        return file
+    }
+}
 
