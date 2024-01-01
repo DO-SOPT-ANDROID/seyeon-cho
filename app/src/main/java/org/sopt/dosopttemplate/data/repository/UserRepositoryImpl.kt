869d68fab7a2f4bb02aaf3c29c62607c85976e37
crownjoe <crownjoe@naver.com>
2024-01-01 22:36:10 +0900

    [feat] 오류 수정

--- app/src/main/java/org/sopt/dosopttemplate/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/repository/UserRepositoryImpl.kt
@@ -1,12 +1,10 @@
 package org.sopt.dosopttemplate.data.repository
 
-import dagger.hilt.android.lifecycle.HiltViewModel
 import org.sopt.dosopttemplate.domain.repository.UserRepository
 import org.sopt.dosopttemplate.data.datasource.UserDataSource
 import org.sopt.dosopttemplate.domain.entity.UserEntity
 import javax.inject.Inject
 
-@HiltViewModel
 class UserRepositoryImpl @Inject constructor(
     private val userDataSource: UserDataSource
     ): UserRepository {
