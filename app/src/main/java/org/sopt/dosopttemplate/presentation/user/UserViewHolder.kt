c9a0eebbebe5873fcc83ce94ce686cb45f7b8ae7
crownjoe <crownjoe@naver.com>
2024-01-01 21:51:27 +0900

    [feat] presentation UserViewHolder, UserViewModel

--- app/src/main/java/org/sopt/dosopttemplate/presentation/user/UserViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/user/UserViewHolder.kt
@@ -1,16 +1,16 @@
-package org.sopt.dosopttemplate.user
+package org.sopt.dosopttemplate.presentation.user
 
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import org.sopt.dosopttemplate.data.user.ResponseUserData
 import org.sopt.dosopttemplate.databinding.ItemUserBinding
+import org.sopt.dosopttemplate.domain.entity.UserEntity
 
 
 class UserViewHolder(
     private val binding: ItemUserBinding
 ): RecyclerView.ViewHolder(binding.root) {
 
-    fun onBind(item: ResponseUserData) {
+    fun onBind(item: UserEntity) {
         binding.tvUserName.text = item.first_name
         binding.tvUserEmail.text = item.email
         Glide.with(binding.root.context)
---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/user/UserViewModel.kt
@@ -0,0 +1,40 @@
+package org.sopt.dosopttemplate.presentation.user
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.sopt.dosopttemplate.domain.entity.UserEntity
+import org.sopt.dosopttemplate.domain.repository.UserRepository
+import org.sopt.dosopttemplate.presentation.login.UiState
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor(
+    private val repository: UserRepository,
+) : ViewModel() {
+    private val _userState = MutableStateFlow<UiState<List<UserEntity>>>(UiState.Loading)
+    val userState: StateFlow<UiState<List<UserEntity>>> get() = _userState
+
+    fun getUserFromServer(page: Int) {
+        _userState.value = UiState.Loading
+        viewModelScope.launch {
+            repository.getUserList(page)
+                .onSuccess { userList ->
+                    val User = userList.map { userEntity ->
+                        UserEntity(
+                            avatar = userEntity.avatar,
+                            email = userEntity.email,
+                            first_name = userEntity.first_name,
+                        )
+                    }
+                    _userState.value = UiState.Success(User)
+                }
+                .onFailure { exception ->
+                    _userState.value = UiState.Error
+                }
+        }
+    }
+}
